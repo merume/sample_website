cf836e739ae4b384a8b63c2ea40f1d54ae047241
Masaki Sato <merume@gmail.com>
2013-02-05 11:41:43 +0900

    [#43820195] routes.rb の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 SampleWebsite::Application.routes.draw do
   root :to => 'pages#home'
-  # get "pages/home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
